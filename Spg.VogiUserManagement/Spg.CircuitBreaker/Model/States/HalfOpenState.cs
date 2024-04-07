d29102829514d0a5fcafbcf4b893be7966d93460
Mirac Fidanci <miracfidanci1@gmail.com>
2024-04-07 21:55:02 +0200

    BreakitCircler

---
+++ Spg.VogiUserManagement/Spg.CircuitBreaker/Model/States/HalfOpenState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.CircuitBreaker.Model.States
+{
+    public class HalfOpenState : CircuitBreakerState
+    {
+        private readonly DateTime _openDateTime;
+
+        public HalfOpenState(CircuitBreaker circuitBreaker)
+            : base(circuitBreaker)
+        {
+            _openDateTime = DateTime.UtcNow;
+        }
+
+        public override CircuitBreaker ProtectedCodeIsExecuting()
+        {
+            base.ProtectedCodeIsExecuting();
+            Update();
+            return _circuitBreaker;
+        }
+
+        public override CircuitBreakerState Update()
+        {
+            base.Update();
+            if (DateTime.UtcNow >= _openDateTime + _circuitBreaker.Timeout)
+            {
+                return _circuitBreaker.MoveToHalfOpenState();
+            }
+            return this;
+        }
+    }
+}
